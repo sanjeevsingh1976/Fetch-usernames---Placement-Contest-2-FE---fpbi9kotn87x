70803b20919f45832b90fb39184b8a961b39b42e
sanjeev singh <sanjeev.rajput1976@gmail.com>
2022-10-09 11:02:17 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 const App = () => {
-const [name,setname] =useState("")
+  const [name, setname] = useState("");
 
-const changeInput = (e) => {
-  setname(e.target.value);
-
-useEffect(()=>{
-  axios.get(`https://content.newtonschool.co/v1/pr/main/users${name}`).then(res =>{
-    console.log(res)
-  }).catch(err =>{
-    console.log(err)
-  })
-})
-  return (
-    <div className="App">
-      <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" value={name} onChange={changeInput}/>
-      <p id="name">{name}</p>
-    </div>
-  );
-}
-
-}
+  const changeInput = (e) => {
+    setname(e.target.value);
+    // fecthing data
+    useEffect(() => {
+      axios
+        .get(`https://content.newtonschool.co/v1/pr/main/users${name}`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    return (
+      <div className="App">
+        <h1 id="text">Type a number between 1 and 10</h1>
+        <input id="input" value={name} onChange={changeInput} />
+        <p id="name">{name}</p>
+      </div>
+    );
+  };
+};
 export default App;
